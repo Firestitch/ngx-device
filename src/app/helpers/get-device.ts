e59fb990080e8340a2f4feb815bbb8b01dfa99ce
Raymond Gigliotti <ray@firestitch.com>
2024-10-02 15:16:20 -0400

    added @firestitch/html

--- src/app/helpers/get-device.ts
+++ src/app/helpers/get-device.ts
@@ -1,15 +1,23 @@
 import { UAParser } from 'ua-parser-js';
 
-export function getDevice(userAgent): { mobile: boolean } {
-  let parser = new UAParser(userAgent);
+export function getDevice(userAgent): { 
+  mobile: boolean,
+  type: string,
+  vendor: string,
+  model: string,
+} {
+  const parser = new UAParser(userAgent);
 
-  let parserResults = parser.getResult();
+  const parserResults = parser.getResult();
 
   const mobile = ['mobile', 'tablet', 'smarttv', 'wearable', 'embedded']
     .includes(parserResults?.device.type);
   
   return {
-    mobile
+    mobile,
+    type: parserResults?.device.type || null,
+    vendor: parserResults?.device.vendor || null,
+    model: parserResults?.device.model || null,
   };
 }
 
